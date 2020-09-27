e2b4c4fee05a26270fa928ac2f46b5f277d4280f
kantegory <davidobryakov@gmail.com>
2020-09-27 17:06:34 +0300

    Update mailer logic

--- scripts/mailer.js
+++ scripts/mailer.js
@@ -36,31 +36,36 @@ sendRequestBtn.addEventListener('click', async (event) => {
   let phone = form.querySelector('[name="recipient-phone"]');
   let message = form.querySelector('[name="message-text"]');
 
-  let formData = { name: name.value, email: email.value, phone: phone.value, message: message.value };
-  formData = JSON.stringify(formData);
+  let msg = `Имя отправителя: ${name.value}\nEmail: ${email.value}\nНомер телефона: ${phone.value}\nСообщение: ${message.value}`;
+  msg = JSON.stringify({ msg: msg });
 
   // check is email valid
   let isEmailValid = validateEmail(email.value);
 
   if (!name.value) {
     showErr(name, "Поле Имя обязательно к заполнению");
+    return;
   } else if (!phone.value) {
     showErr(phone, "Поле Телефон обязательно к заполнению");
+    return;
   } else if (!email.value) {
     showErr(email, "Поле Email обязательно к заполнению");
+    return;
   } else if (!isEmailValid) {
     showErr(email, "Поле Email заполнено некорректно");
+    return;
   } else if (!message.value) {
     showErr(message, "Поле Сообщение обязательно к заполнению");
+    return;
   }
 
   // sending form
   let result = await fetch("https://push.kubteh.ru/lp/mailer/sendmsg", {
     method: "POST",
     headers: {
-      'Content-type': 'application/json'
+      'Content-Type': 'application/json'
     },
-    body: formData,
+    body: msg,
     mode: 'cors',
     cache: 'no-cache',
     credentials: 'same-origin'
@@ -70,5 +75,38 @@ sendRequestBtn.addEventListener('click', async (event) => {
 
   if (result.success) {
     // success logic is there
+
+    // reset fields values
+    name.value = "";
+    name.setCustomValidity("");
+    name.required = false;
+    email.value = "";
+    email.setCustomValidity("");
+    email.required = false;
+    phone.value = "";
+    phone.setCustomValidity("");
+    phone.required = false
+    message.value = "";
+    message.setCustomValidity("");
+    message.required = false;
+
+    // close current modal
+    document.querySelector("#sendRequest .close").click();
+
+    // show success modal
+    // open logic
+    document.querySelector('.shadow').style.display = 'block';
+    document.querySelector('#success').style.display = 'block';
+    document.querySelector('#success').classList.add('show');
+    document.querySelector('html').classList.add('modal-open');
+
+    // close logic
+    Array.from(document.querySelectorAll('#success button')).map((elem) =>
+      elem.addEventListener('click', () => {
+        document.querySelector('.shadow').style.display = 'none';
+        document.querySelector('#success').classList.remove('show');
+        document.querySelector('#success').style.display = 'none';
+        document.querySelector('html').classList.remove('modal-open');
+      }));
   }
 })
--- scripts/modals.js
+++ scripts/modals.js
@@ -6,12 +6,12 @@ openModal.addEventListener('click', () => {
   document.querySelector('#sendRequest').style.display = 'block';
   document.querySelector('#sendRequest').classList.add('show');
   document.querySelector('html').classList.add('modal-open');
-  
+
   // close logic
   document.querySelector('#sendRequest .close').addEventListener('click', () => {
     document.querySelector('.shadow').style.display = 'none';
     document.querySelector('#sendRequest').classList.remove('show');
     document.querySelector('#sendRequest').style.display = 'none';
     document.querySelector('html').classList.remove('modal-open');
-  })
+  });
 });
