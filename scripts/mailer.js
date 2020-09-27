ed4770b92eb0136864f60f22b0fae8d366d13ee2
kantegory <davidobryakov@gmail.com>
2020-09-27 14:22:57 +0300

    Add form validating and getting values from it

---
+++ scripts/mailer.js
@@ -0,0 +1,58 @@
+const sendRequestBtn = document.querySelector('#sendRequestBtn');
+const form = document.querySelector('#sendRequest form');
+let errBlock = document.querySelector('.error');
+
+// show err function
+const showErr = (field, msg) => {
+  field.setCustomValidity(msg);
+  errBlock.innerHTML = msg;
+  errBlock.style.display = "block";
+  setTimeout(() => {
+    errBlock.style.display = "none";
+    field.setCustomValidity("");
+  }, 2500);
+}
+
+// validate russian phone number
+let phoneMask = IMask(
+  document.querySelector('[name="recipient-phone"]'), {
+    mask: '+{7}(000)000-00-00'
+  }
+);
+
+// validate email function
+const validateEmail = (email) => {
+  const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return re.test(email);
+}
+
+sendRequestBtn.addEventListener('click', (event) => {
+  // cancel form submitting
+  event.preventDefault();
+
+  // get formData
+  let name = form.querySelector('[name="recipient-name"]');
+  let email = form.querySelector('[name="recipient-email"]');
+  let phone = form.querySelector('[name="recipient-phone"]');
+  let message = form.querySelector('[name="message-text"]');
+
+  let formData = { name: name.value, email: email.value, phone: phone.value, message: message.value };
+
+  console.log(formData);
+
+  // check is email valid
+  let isEmailValid = validateEmail(email.value);
+
+  if (!name.value) {
+    showErr(name, "Поле Имя обязательно к заполнению");
+  } else if (!phone.value) {
+    showErr(phone, "Поле Телефон обязательно к заполнению");
+  } else if (!email.value) {
+    showErr(email, "Поле Email обязательно к заполнению");
+  } else if (!isEmailValid) {
+    showErr(email, "Поле Email заполнено некорректно");
+  } else if (!message.value) {
+    showErr(message, "Поле Сообщение обязательно к заполнению");
+  }
+
+})
